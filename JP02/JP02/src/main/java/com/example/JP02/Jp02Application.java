c240f4405502b7dbdce526a339c543b003f7a561
shabeerrock <shabeer.rock1@gmail.com>
2025-09-01 00:04:56 +0530

    weather App

---
+++ JP02/JP02/src/main/java/com/example/JP02/Jp02Application.java
@@ -0,0 +1,13 @@
+package com.example.JP02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jp02Application.class, args);
+	}
+
+}
---
+++ JP02/JP02/src/test/java/com/example/JP02/Jp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JP02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jp02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
